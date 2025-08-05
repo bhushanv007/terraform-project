f5ba8b4c5c57613a90b6079f12d99ba8b610ef84
bhushanv007 <87693491+bhushanv007@users.noreply.github.com>
2025-08-05 13:14:32 +0530

    Update ec2file.tf
    removed some sesetive content

--- ec2file.tf
+++ ec2file.tf
@@ -1,15 +1,6 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAX2DZEEBODY3PG5FG"
-  secret_key = "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"
-}
 
 resource "aws_instance" "web" {
   ami           = "ami-02457590d33d576c3"
   instance_type = "t2.micro"
-/*
-  tags = {
-    Name = "HelloWorld"
-  }
-*/
+
 }
